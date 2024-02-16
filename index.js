51782f77b1889fec1835ff05045c23574c00dee9
Shaurya Dhaka <138813918+shauryadhaka0027@users.noreply.github.com>
2024-02-16 12:30:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const cookieParser = require('cookie-parser');
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-     origin: "*",
+     origin: "https://65cf067765788b87fb36c015--celebrated-lamington-79bbb8.netlify.app",
     credentials: true
   }));
  
