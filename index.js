560ffc994c8dfa34711011d9edb5bee735558349
Shaurya Dhaka <138813918+shauryadhaka0027@users.noreply.github.com>
2024-02-16 13:25:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
 origin:["https://shaurya-wayfair.netlify.app", "http://127.0.0.1:5173"],
-
+      methods:["GET","POST"],
     credentials: true
   }));
  
